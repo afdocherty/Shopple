bb2cc3fba00313de41c8b2b2fb83a7727e77ec0c
Kathleen Hu <kch4@rice.edu>
2017-07-11 14:10:50 -0700

    First commit

---
+++ app/src/main/java/com/example/kathu228/shoplog/ShoplistActivity.java
@@ -0,0 +1,13 @@
+package com.example.kathu228.shoplog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoplistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoplist);
+    }
+}
