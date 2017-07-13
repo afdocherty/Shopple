dee38ce5086ff04b72166db97091e4bd87dc787b
afdocherty <foster.docherty@gmail.com>
2017-07-13 11:38:07 -0700

    More informative log messages for querying

--- app/src/main/java/com/example/kathu228/shoplog/Fragments/ItemlistFragment.java
+++ app/src/main/java/com/example/kathu228/shoplog/Fragments/ItemlistFragment.java
@@ -150,8 +150,11 @@ public class ItemlistFragment extends Fragment implements ItemAdapter.ItemAdapte
                         public void done(java.util.List<ParseObject> results, ParseException e) {
                             if (e != null) {
                                 // There was an error
+                                Log.d("LoginActivity", "Segment not found");
+                                e.printStackTrace();
                             } else {
                                 // results have all the segments in the list
+                                Log.d("LoginActivity", "Segment found");
                                 segTest = (Segment) results.get(0);
                                 Log.d("LoginActivity", "listTest segment name: " + segTest.getName());
                                 ParseRelation<ParseObject> relationSegmentToItem = segTest.getRelation("items");
@@ -160,8 +163,13 @@ public class ItemlistFragment extends Fragment implements ItemAdapter.ItemAdapte
                                     public void done(java.util.List<ParseObject> results, ParseException e) {
                                         if (e != null) {
                                             // There was an error
+                                            Log.d("LoginActivity", "Items not found");
+                                            e.printStackTrace();
                                         } else {
                                             // results have all the items in the segment
+                                            Log.d("LoginActivity", "Items found");
+                                            
+                                            // Add the items to the items arraylist
                                             for (ParseObject parseObject : results) {
                                                 items.add((Item) parseObject);
                                             }
