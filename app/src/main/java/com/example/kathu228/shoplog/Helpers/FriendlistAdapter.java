996abf1b7cd986dcc55ebf6fe3dde4c5611efb30
Kathleen Hu <kch4@rice.edu>
2017-07-13 17:50:37 -0700

    Changed text and margin sizes

--- app/src/main/java/com/example/kathu228/shoplog/Helpers/FriendlistAdapter.java
+++ app/src/main/java/com/example/kathu228/shoplog/Helpers/FriendlistAdapter.java
@@ -3,7 +3,6 @@ package com.example.kathu228.shoplog.Helpers;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.TextView;
 
 import com.example.kathu228.shoplog.Fragments.FriendlistFragment;
 import com.example.kathu228.shoplog.R;
@@ -34,20 +33,17 @@ public class FriendlistAdapter extends BaseAdapter<FriendlistAdapter.ViewHolder,
     public void onBindViewHolder(ViewHolder holder, int position) {
         String name = mlist.get(position);
 
-        holder.tvName.setText(name);
         holder.cbName.setText(name);
     }
 
     // ViewHolder Class
     public class ViewHolder extends BaseAdapter.ViewHolder{
 
-        public TextView tvName;
         public CheckBox cbName;
 
         public ViewHolder(View itemView){
             super(itemView);
 
-            tvName = (TextView) itemView.findViewById(R.id.tvName);
             cbName = (CheckBox) itemView.findViewById(R.id.cbName);
             cbName.setChecked(false);
 
