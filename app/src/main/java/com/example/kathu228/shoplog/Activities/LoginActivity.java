ccbc576da0e2eebd4af5515f2a8efa5f44290f79
afdocherty <foster.docherty@gmail.com>
2017-07-14 16:45:50 -0700

    Merging into MVP

--- app/src/main/java/com/example/kathu228/shoplog/Activities/LoginActivity.java
+++ app/src/main/java/com/example/kathu228/shoplog/Activities/LoginActivity.java
@@ -76,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                                     }
                                 }
                             });
-//                }
+                }
             }
         });
     }
