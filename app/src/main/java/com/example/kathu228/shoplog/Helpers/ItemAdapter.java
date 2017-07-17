1246e72f186dad0d520f7eef556482e788b5360a
Kathleen Hu <kch4@rice.edu>
2017-07-17 14:45:10 -0700

    Updated swipe delete to delete item from database

--- app/src/main/java/com/example/kathu228/shoplog/Helpers/ItemAdapter.java
+++ app/src/main/java/com/example/kathu228/shoplog/Helpers/ItemAdapter.java
@@ -1,6 +1,5 @@
 package com.example.kathu228.shoplog.Helpers;
 
-import android.os.CountDownTimer;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
@@ -67,8 +66,9 @@ public class ItemAdapter extends BaseAdapter<ItemAdapter.ViewHolder,Item> implem
     // Todo: remove item from database
     @Override
     public void onItemDismiss(int position) {
-        mlist.remove(position);
-        notifyItemRemoved(position);
+        Item item = mlist.get(position);
+        deleteItem(position);
+        deleteItemFromList(item);
     }
 
     // Provide a direct reference to each of the views within a data item
@@ -104,126 +104,125 @@ public class ItemAdapter extends BaseAdapter<ItemAdapter.ViewHolder,Item> implem
                 item.setChecked(true);
                 item.saveInBackground();
 
-                // Delays a little after checking box, then deletes
-                new CountDownTimer(700, 1000) {
-                    public void onFinish() {
-                        // When timer is finished
-                        // Execute your code here
-                        deleteItemFromList(item);
-                        deleteItem(item, position, v);
-                    }
-
-                    public void onTick(long millisUntilFinished) {
-                        // millisUntilFinished    The amount of time until finished.
-                    }
-                }.start();
-                ;
+//                // Delays a little after checking box, then deletes
+//                new CountDownTimer(700, 1000) {
+//                    public void onFinish() {
+//                        // When timer is finished
+//                        // Execute your code here
+//                        deleteItemFromList(item);
+//                        deleteItem(item, position, v);
+//                    }
+//
+//                    public void onTick(long millisUntilFinished) {
+//                        // millisUntilFinished    The amount of time until finished.
+//                    }
+//                }.start();
+//                ;
 
             }
         }
+    }
 
-        // Remove an item from the MVP list
-        private void deleteItemFromList (final Item item) {
-            // MVP Hack to jump straight to Segment - TODO change
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Segment");
-            query.findInBackground(new FindCallback<ParseObject>() {
-                @Override
-                public void done(List<ParseObject> objects, ParseException e) {
-                    if (e == null) {
-                        Log.d("ItemListFragment", "Segment found");
-                        // Grab the first segment (for MVP) - TODO change
-                        ParseRelation<ParseObject> relationSegmentToItem = objects.get(0).getRelation("items");
-                        relationSegmentToItem.remove(item);
-                        objects.get(0).saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                if (e == null) {
-                                    Log.d("ItemListFragment", "Item removed!");
-                                } else {
-                                    Log.d("ItemListFragment", "Item not removed. Error: " + e.toString());
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                        item.deleteInBackground(new DeleteCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                if (e == null) {
-                                    Log.d("ItemListFragment", "Item deleted");
-                                }
-                                else {
-                                    Log.d("ItemListFragment", "Item not deleted. Error: " + e.toString());
-                                }
-                            }
-                        });
-
-                    } else {
-                        Log.d("ItemListFragment", "Segment not found. Error: " + e.toString());
-                    }
-
-                }
-            });
-        }
-
-        // deletes if checkbox is checked, and allows undo deletion
-        public void deleteItem(final Item item, final int position, View v){
-            // Todo: update remove item
-            mlist.remove(position);
-            notifyItemRemoved(position);
-            undoDelete(item, position, v);
-
-        }
-
-        // Add an item to the MVP list
-        private void addItemToList (final Item item) {
-            // MVP Hack to jump straight to Segment - TODO change
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Segment");
-            query.findInBackground(new FindCallback<ParseObject>() {
-                @Override
-                public void done(List<ParseObject> objects, ParseException e) {
-                    if (e == null) {
-                        Log.d("ItemListFragment", "Segment found");
-                        // Grab the first segment (for MVP) - TODO change
-                        ParseRelation<ParseObject> relationSegmentToItem = objects.get(0).getRelation("items");
-                        relationSegmentToItem.add(item);
-                        objects.get(0).saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                if (e == null) {
-                                    Log.d("ItemListFragment", "Item added!");
-                                } else {
-                                    Log.d("ItemListFragment", "Item not added. Error: " + e.toString());
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                    } else {
-                        Log.d("ItemListFragment", "Segment not found. Error: " + e.toString());
-                    }
-                }
-            });
-        }
-
-        // if click undo in snackbar, item will reappear in list unchecked
-        public void undoDelete(final Item item, final int position, View v){
-            Snackbar.make(v, item.getBody()+" deleted", Snackbar.LENGTH_LONG)
-                    .setAction("Undo", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            cbItem.setChecked(false);
-                            item.setChecked(false);
-                            // Todo: update add item later
-                            mlist.add(position, item);
-                            addItemToList(item);
-                            notifyItemInserted(position);
-                            Snackbar snackbar1 = Snackbar.make(v, item.getBody()+" restored!", Snackbar.LENGTH_SHORT);
-                            snackbar1.show();
-                        }
-                    })
-                    .show();
-        }
-
-
+    // deletes if checkbox is checked, and allows undo deletion
+    public void deleteItem(final int position){
+        // Todo: update remove item
+        mlist.remove(position);
+        notifyItemRemoved(position);
+        //undoDelete(item, position, v);
 
     }
+
+    // Remove an item from the MVP list
+    private void deleteItemFromList (final Item item) {
+        // MVP Hack to jump straight to Segment - TODO change
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Segment");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    Log.d("ItemListFragment", "Segment found");
+                    // Grab the first segment (for MVP) - TODO change
+                    ParseRelation<ParseObject> relationSegmentToItem = objects.get(0).getRelation("items");
+                    relationSegmentToItem.remove(item);
+                    objects.get(0).saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Log.d("ItemListFragment", "Item removed!");
+                            } else {
+                                Log.d("ItemListFragment", "Item not removed. Error: " + e.toString());
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    item.deleteInBackground(new DeleteCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Log.d("ItemListFragment", "Item deleted");
+                            }
+                            else {
+                                Log.d("ItemListFragment", "Item not deleted. Error: " + e.toString());
+                            }
+                        }
+                    });
+
+                } else {
+                    Log.d("ItemListFragment", "Segment not found. Error: " + e.toString());
+                }
+
+            }
+        });
+    }
+
+    // Add an item to the MVP list
+    private void addItemToList (final Item item) {
+        // MVP Hack to jump straight to Segment - TODO change
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Segment");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    Log.d("ItemListFragment", "Segment found");
+                    // Grab the first segment (for MVP) - TODO change
+                    ParseRelation<ParseObject> relationSegmentToItem = objects.get(0).getRelation("items");
+                    relationSegmentToItem.add(item);
+                    objects.get(0).saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Log.d("ItemListFragment", "Item added!");
+                            } else {
+                                Log.d("ItemListFragment", "Item not added. Error: " + e.toString());
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } else {
+                    Log.d("ItemListFragment", "Segment not found. Error: " + e.toString());
+                }
+            }
+        });
+    }
+
+    // if click undo in snackbar, item will reappear in list unchecked
+    public void undoDelete(final Item item, final int position, View v){
+        Snackbar.make(v, item.getBody()+" deleted", Snackbar.LENGTH_LONG)
+                .setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        cbItem.setChecked(false);
+                        item.setChecked(false);
+                        // Todo: update add item later
+                        mlist.add(position, item);
+                        addItemToList(item);
+                        notifyItemInserted(position);
+                        Snackbar snackbar1 = Snackbar.make(v, item.getBody()+" restored!", Snackbar.LENGTH_SHORT);
+                        snackbar1.show();
+                    }
+                })
+                .show();
+    }
+
+
 }
