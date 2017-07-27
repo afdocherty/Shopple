60f23457d1f8bd5f86414e5af3f4cdf6677b92ca
afdocherty <foster.docherty@gmail.com>
2017-07-27 15:39:23 -0700

    Modal Overlay for adding collaborators (CollaboratorsDialogFragment)

--- app/src/main/java/com/example/kathu228/shoplog/Fragments/ItemlistFragment.java
+++ app/src/main/java/com/example/kathu228/shoplog/Fragments/ItemlistFragment.java
@@ -92,93 +92,99 @@ public class ItemlistFragment extends Fragment implements SegmentDialogFragment.
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_itemlist, container, false);
-        // find the RecyclerView
-        rvItems = (RecyclerView) v.findViewById(R.id.rvItem);
+
+        try {
+            // get id of shoplist
+            String shopListObjectId = getArguments().getString(ShopList.SHOPLIST_TAG);
+            Log.d("ItemlistFragment", "objId: " + shopListObjectId);
+            shopList = ShopList.getShopListById(shopListObjectId);
+
+            // find the RecyclerView
+            rvItems = (RecyclerView) v.findViewById(R.id.rvItem);
 //        rvCompleted = (RecyclerView) v.findViewById(R.id.rvCompleted);
-        // initialize the array of items
-        items = new ArrayList<>();
+            // initialize the array of items
+            items = new ArrayList<>();
 
-        // get id of shoplist
-        String shopListObjectId = getArguments().getString(ShopList.SHOPLIST_TAG);
-        Log.d("ItemlistFragment", "objId: " + shopListObjectId);
-        shopList = ShopList.getShopListById(shopListObjectId);
+            // construct the adapter
+            itemAdapter = new ItemAdapter(items, shopList, v);
+            // Set layout manager to position the items
+            rvItems.setLayoutManager(new LinearLayoutManager(getContext()));
+            // Attach the adapter to the recyclerview to populate items
+            rvItems.setAdapter(itemAdapter);
 
-        // construct the adapter
-        itemAdapter = new ItemAdapter(items, shopList, v);
-        // Set layout manager to position the items
-        rvItems.setLayoutManager(new LinearLayoutManager(getContext()));
-        // Attach the adapter to the recyclerview to populate items
-        rvItems.setAdapter(itemAdapter);
-
-        // Shift focus to dummy view to prevent auto-focusing on EditText
-        llDummy = (LinearLayout) v.findViewById(R.id.llDummy);
-        llDummy.setFocusableInTouchMode(true);
-        if (!getArguments().getBoolean(ShopList.SHOPLIST_NEW_TAG)) {
-            llDummy.requestFocus();
-        }
-
-        etAddItem = (EditText) v.findViewById(R.id.etAddItem);
-        ibAddItem = (ImageButton) v.findViewById(R.id.ibAddItem);
-
-        // Put onclicklistener onto add button to add item to list
-        ibAddItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItem();
+            // Shift focus to dummy view to prevent auto-focusing on EditText
+            llDummy = (LinearLayout) v.findViewById(R.id.llDummy);
+            llDummy.setFocusableInTouchMode(true);
+            if (!getArguments().getBoolean(ShopList.SHOPLIST_NEW_TAG)) {
+                llDummy.requestFocus();
             }
-        });
 
-        // Create add FAB and force visible (Note: must be defined before etAddItem OnEditorActionListener!)
-        fabAddSegment = (FloatingActionButton) v.findViewById(R.id.fabAddSegment);
-        fabAddSegment.setVisibility(View.VISIBLE);
-        fabAddSegment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSegmentDialog();
-            }
-        });
+            etAddItem = (EditText) v.findViewById(R.id.etAddItem);
+            ibAddItem = (ImageButton) v.findViewById(R.id.ibAddItem);
 
-        // Adds item from edittext if press enter or done on keyboard
-        etAddItem.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+            // Put onclicklistener onto add button to add item to list
+            ibAddItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
                     addItem();
                 }
-                return false;
-            }
-        });
+            });
 
-        // 3rd-party workaround to catch keyboard open/close events
-        KeyboardVisibilityEvent.setEventListener(
-                getActivity(),
-                new KeyboardVisibilityEventListener() {
-                    @Override
-                    public void onVisibilityChanged(boolean isOpen) {
-                        if (isOpen) {
-                            fabAddSegment.setVisibility(View.INVISIBLE);
-                        } else {
-                            fabAddSegment.setVisibility(View.VISIBLE);
-                        }
+            // Create add FAB and force visible (Note: must be defined before etAddItem OnEditorActionListener!)
+            fabAddSegment = (FloatingActionButton) v.findViewById(R.id.fabAddSegment);
+            fabAddSegment.setVisibility(View.VISIBLE);
+            fabAddSegment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showSegmentDialog();
+                }
+            });
+
+            // Adds item from edittext if press enter or done on keyboard
+            etAddItem.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                        addItem();
                     }
-                });
+                    return false;
+                }
+            });
 
-        // attaches touchHelper to the recyclerview
-        ItemTouchHelper.Callback callback =
-                new SimpleItemTouchHelperCallback(itemAdapter);
-        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
-        touchHelper.attachToRecyclerView(rvItems);
+            // 3rd-party workaround to catch keyboard open/close events
+            KeyboardVisibilityEvent.setEventListener(
+                    getActivity(),
+                    new KeyboardVisibilityEventListener() {
+                        @Override
+                        public void onVisibilityChanged(boolean isOpen) {
+                            if (isOpen) {
+                                fabAddSegment.setVisibility(View.INVISIBLE);
+                            } else {
+                                fabAddSegment.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    });
 
-        //find the swipe refresh layout and add the onRefreshListener
-        swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
-        //swipeContainer.setColorSchemeResources(R.color.);
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                //call method to repopulate the timeline
-                addItems();
-                swipeContainer.setRefreshing(false);
-            }
-        });
+            // attaches touchHelper to the recyclerview
+            ItemTouchHelper.Callback callback =
+                    new SimpleItemTouchHelperCallback(itemAdapter);
+            ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+            touchHelper.attachToRecyclerView(rvItems);
+
+            //find the swipe refresh layout and add the onRefreshListener
+            swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
+            //swipeContainer.setColorSchemeResources(R.color.);
+            swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    //call method to repopulate the timeline
+                    addItems();
+                    swipeContainer.setRefreshing(false);
+                }
+            });
+        } catch (NullPointerException e) {
+            Log.d("ListDetailsFragment", "ShopList not found by id");
+            throw e;
+        }
 
         return v;
     }
