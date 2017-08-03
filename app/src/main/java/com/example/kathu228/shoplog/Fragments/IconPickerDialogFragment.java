ba3abbb4e2ec8c54e5b2d3c915e605f0b1d2bd9a
afdocherty <foster.docherty@gmail.com>
2017-08-03 13:14:59 -0700

    Finished icon picker, polished list details notifications

--- app/src/main/java/com/example/kathu228/shoplog/Fragments/IconPickerDialogFragment.java
+++ app/src/main/java/com/example/kathu228/shoplog/Fragments/IconPickerDialogFragment.java
@@ -7,9 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.kathu228.shoplog.Models.Item;
 import com.example.kathu228.shoplog.Models.ShopList;
 import com.example.kathu228.shoplog.R;
 
@@ -19,9 +19,19 @@ import com.example.kathu228.shoplog.R;
 
 public class IconPickerDialogFragment extends DialogFragment {
     private TextView mTitle;
-    private Button mYes;
-    private Button mNo;
+    private Button mConfirm;
+    private Button mCancel;
     private IconPickerDialogListener mListener;
+    private ImageView groceryBagIcon;
+    private ImageView soloCupIcon;
+    private ImageView bbqGrillIcon;
+    private int iconNum;
+
+    public static final int ALPHA_FULL = 255;
+    public static final int ALPHA_DIMMED = 86;
+    public static final int ICON_NUM_GROCERY = 0;
+    public static final int ICON_NUM_PARTY = 1;
+    public static final int ICON_NUM_BBQ = 2;
 
     public void setListener(IconPickerDialogListener listener) {
         mListener = listener;
@@ -29,18 +39,18 @@ public class IconPickerDialogFragment extends DialogFragment {
 
     // Defines the listener interface
     public interface IconPickerDialogListener {
-        void onFinishYesNoDialog(Boolean yes, String title, ShopList mshopList);
+        void onFinishIconPickerDialog(Boolean yes, int iconNum, String title, ShopList mshopList);
     }
 
     // Call this method to send the data back to the parent fragment
-    public void sendBackResult(Boolean yes, String title, @Nullable ShopList mshopList) {
+    public void sendBackResult(Boolean yes, int iconNum, String title, @Nullable ShopList mshopList) {
         // Notice the use of `getTargetFragment` which will be set when the dialog is displayed
         //YesNoDialogListener listener = (YesNoDialogListener) getTargetFragment();
-        mListener.onFinishYesNoDialog(yes, title, mshopList);
+        mListener.onFinishIconPickerDialog(yes, iconNum, title, mshopList);
         dismiss();
     }
 
-    public static IconPickerDialogFragment newInstance(String title, String question, @Nullable Item mitem, @Nullable ShopList mshopList) {
+    public static IconPickerDialogFragment newInstance(String title, @Nullable ShopList mshopList) {
         IconPickerDialogFragment frag = new IconPickerDialogFragment();
         Bundle args = new Bundle();
         args.putString(YesNoDialogFragment.TITLE, title);
@@ -59,47 +69,111 @@ public class IconPickerDialogFragment extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Get field from view
-        mTitle = (TextView) view.findViewById(R.id.tvTitle);
-        mNo = (Button) view.findViewById(R.id.bNo);
-        mYes = (Button) view.findViewById(R.id.bYes);
-        // Fetch arguments from bundle and set title/question
+        // Fetch arguments from bundle and set title
         final String title = getArguments().getString(YesNoDialogFragment.TITLE);
         final ShopList shopList = getArguments().getParcelable(YesNoDialogFragment.SHOPLIST);
-        mTitle.setText(title);
-        // See if user selects yes or no
-        mNo.setOnClickListener(new View.OnClickListener() {
+
+        // Get field from view
+        mTitle = (TextView) view.findViewById(R.id.tvTitle);
+        mCancel = (Button) view.findViewById(R.id.bCancel);
+        mConfirm = (Button) view.findViewById(R.id.bConfirm);
+
+        // Get icon number
+        iconNum = shopList.getIconNum();
+        groceryBagIcon = (ImageView) view.findViewById(R.id.icon_groceries);
+        groceryBagIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendBackResult(false, title, shopList);
+                setFocusToGrocery();
             }
         });
-        mYes.setOnClickListener(new View.OnClickListener() {
+        soloCupIcon = (ImageView) view.findViewById(R.id.icon_party);
+        soloCupIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendBackResult(true, title, shopList);
+                setFocusToParty();
+            }
+        });
+        bbqGrillIcon = (ImageView) view.findViewById(R.id.icon_bbq);
+        bbqGrillIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFocusToBBQ();
+            }
+        });
+        setCurrentFocus(iconNum);
+
+        mTitle.setText(title);
+        // See if user selects yes or no
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBackResult(false, iconNum, title, shopList);
+            }
+        });
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBackResult(true, iconNum, title, shopList);
             }
         });
     }
 
-    public void onActivityCreated(Bundle savedInstanceState){
-        super.onActivityCreated(savedInstanceState);
-        final String title = getArguments().getString(YesNoDialogFragment.TITLE);
-        final ShopList shopList = getArguments().getParcelable(YesNoDialogFragment.SHOPLIST);
-        mNo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendBackResult(false,title,shopList);
-                //mListener.onFinishYesNoDialog(false,title, item);
-            }
-        });
-        mYes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendBackResult(true,title,shopList);
-//                mListener.onFinishYesNoDialog(true, title, item);
-            }
-        });
+//    public void onActivityCreated(Bundle savedInstanceState){
+//        super.onActivityCreated(savedInstanceState);
+//        final String title = getArguments().getString(YesNoDialogFragment.TITLE);
+//        final ShopList shopList = getArguments().getParcelable(YesNoDialogFragment.SHOPLIST);
+//
+//        // viewcreated code
+//        mCancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sendBackResult(false, iconNum, title,shopList);
+//                //mListener.onFinishYesNoDialog(false,title, item);
+//            }
+//        });
+//        mConfirm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sendBackResult(true, iconNum, title,shopList);
+////                mListener.onFinishYesNoDialog(true, title, item);
+//            }
+//        });
+//    }
+
+    private void setCurrentFocus(int currIconNum) {
+        switch (currIconNum) {
+            case ICON_NUM_GROCERY:
+                setFocusToGrocery();
+                break;
+            case ICON_NUM_PARTY:
+                setFocusToParty();
+                break;
+            case ICON_NUM_BBQ:
+                setFocusToBBQ();
+                break;
+        }
+    }
+
+    private void setFocusToGrocery() {
+        groceryBagIcon.setImageAlpha(ALPHA_FULL);
+        soloCupIcon.setImageAlpha(ALPHA_DIMMED);
+        bbqGrillIcon.setImageAlpha(ALPHA_DIMMED);
+        iconNum = ICON_NUM_GROCERY;
+    }
+
+    private void setFocusToParty() {
+        groceryBagIcon.setImageAlpha(ALPHA_DIMMED);
+        soloCupIcon.setImageAlpha(ALPHA_FULL);
+        bbqGrillIcon.setImageAlpha(ALPHA_DIMMED);
+        iconNum = ICON_NUM_PARTY;
+    }
+
+    private void setFocusToBBQ() {
+        groceryBagIcon.setImageAlpha(ALPHA_DIMMED);
+        soloCupIcon.setImageAlpha(ALPHA_DIMMED);
+        bbqGrillIcon.setImageAlpha(ALPHA_FULL);
+        iconNum = ICON_NUM_BBQ;
     }
 
 }
